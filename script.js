1725de5aafd94f824615fffaf956135164412896
EugeniaArnaud <euge.arnaud@gmail.com>
2022-04-10 04:09:03 -0300

    mejroas

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY >20){
-$('.navbar').addClass("sticky")
-        }else {
-            $('.navbar').removeClass("sticky")
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').addClass("show")
         }
     })
+
+// slide-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+});
+$('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
+
     // toggle menu bars and x
     $('.menu-btn').click(function(){
         $('.navbar .content_nav').toggleClass("active");
